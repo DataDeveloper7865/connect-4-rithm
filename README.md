7f79fbd2fcb16cf4e566444dcbbd3840b64c38c1
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-02-18 12:08:54 -0800

    make board added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # connect-4-rithm
+
+This game is a replica of the connect four game implemented in HMTL, CSS, Javascript
--- connect4.js
+++ connect4.js
@@ -19,6 +19,9 @@ let board = []; // array of rows, each row is array of cells  (board[y][x])
 
 function makeBoard() {
   // TODO: set "board" to empty HEIGHT x WIDTH matrix array
+  for (let i =0; i < HEIGHT; i++) {
+    board[i] = new Array(WIDTH).fill(null);
+  }
 }
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
